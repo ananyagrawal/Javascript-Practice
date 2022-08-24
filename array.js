1960c8fd50920c04f9d8dee66297008014c4b40c
Anany Agrawal <ananyagrawal698@gmail.com>
2022-08-24 20:01:24 +0530

    Array Practice

---
+++ array.js
@@ -0,0 +1,16 @@
+// Arrays
+const fruits = ['banana', 'apple', 'orange', 'pear']
+console.log(fruits)
+
+// grab the first index
+console.log(fruits[0])
+
+// Array methods (push, slice, indexOf, length)
+fruits.push('grapes')
+
+// Array slice starts from 0 inclusive and up to 2
+console.log(fruits.slice(0,2))
+
+console.log(fruits.indexOf('orange'))
+
+console.log(fruits.length)
