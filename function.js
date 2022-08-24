84b1ab3a28b4e4b27fd58e807ac1777971ba515c
Anany Agrawal <ananyagrawal698@gmail.com>
2022-08-24 19:41:14 +0530

    Function Practice

---
+++ function.js
@@ -0,0 +1,20 @@
+function add(a,b){
+    return a+b
+}
+function sub(a,b){
+    return a-b
+}
+function div(a,b){
+    return a/b
+}
+function mul(a,b){
+    return a*b
+}
+const rem = (a,b) => console.log(a%b)
+let x=4
+let y=21
+console.log(add(x,y))
+console.log(sub(x,y))
+console.log(div(x,y))
+console.log(mul(x,y))
+rem(x,y)
