d1a91c53c411de42143179fea914747fc62e208a
Anany Agrawal <ananyagrawal698@gmail.com>
2022-08-28 12:09:56 +0530

    Create palindromechecker.js

---
+++ palindromechecker.js
@@ -0,0 +1,8 @@
+const isPalindrome = (string) => {
+    let rev=""
+    for(let i=string.length-1; i>=0;i--){
+        rev+=string[i]
+    }
+    return (rev==string)
+}
+console.log(isPalindrome('racecar'))
